65fb519b8a2b8426aae13861195fdf53a4575a95
thomasduffy328 <thomas.duffy@live.com>
2017-06-02 21:36:52 -0400

    Add files via upload

--- pset1.py
+++ pset1.py
@@ -1,31 +1,37 @@
 # part A
 # assume s is a set of lower-string characters
 # write program that enumerates vowels in the string 's'
-#
-# s = str(input('Enter a string: '))
-# vowels = ['a','e','i','o','u']
-# numVowels = 0
-# for char in s:
-#     if char in vowels != False:
-#         numVowels = numVowels + 1
-# print('Number of Vowels:', numVowels)
+
+s = str(input('Enter a string: '))
+# initialize list of vowels and empty vectors
+vowels = ['a','e','i','o','u']
+numVowels = 0
+# loop thru char's asking if each is in vowels, if so + 1
+for char in s:
+    if char in vowels != False:
+        numVowels = numVowels + 1
+# print result
+print('Number of Vowels:', numVowels)
 
 
 # part B
 # still working with s
 # enumerate the instances of 'bob' in the string s
 
-# s = str(input('Enter a string: '))
-# numBobs = 0
-# bobTest = str('')
-# for i in range(0,len(s)):
-#     if s[i] == 'b':
-#         bobTest = s[i:(i+3)]
-#     else:
-#         bobTest = ''
-#     if bobTest == 'bob':
-#         numBobs = numBobs + 1
-# print('Number of times bob occurs is:', numBobs)
+s = str(input('Enter a string: '))
+# initialize empty vectors
+numBobs = 0
+bobTest = str('')
+# loop thru 1 to length s, if b is found, ask if it is beginning of 'bob', if so +1
+for i in range(0,len(s)):
+    if s[i] == 'b':
+        bobTest = s[i:(i+3)]
+    else:
+        bobTest = ''
+    if bobTest == 'bob':
+        numBobs = numBobs + 1
+# print result
+print('Number of times bob occurs is:', numBobs)
 
 # part C
 # first set up nest list matching letter with index
