313e82b52070600ebaea703741de22c81bee1b88
jmsaunders89 <jms815@lehigh.edu>
2017-06-25 18:54:39 -0400

    Update JMS_ps3_iswordguessed.py

--- JMS_ps3_iswordguessed.py
+++ JMS_ps3_iswordguessed.py
@@ -4,10 +4,6 @@ Created on Fri Jun 23 18:55:57 2017
 
 @author: jms815
 """
-
-secretWord2 = 'apple'
-lettersGuessed2 = ['e', 'i', 'k', 'p', 'r', 's']
-
 def isWordGuessed(secretWord, lettersGuessed):  
     secretwordlist = list(secretWord)
     for i in lettersGuessed:
@@ -17,6 +13,3 @@ def isWordGuessed(secretWord, lettersGuessed):
         return True
     else:
         return False
-    
-isWordGuessed(secretWord2, lettersGuessed2)
-#print(isWordGuessed(secretWord, lettersGuessed))
